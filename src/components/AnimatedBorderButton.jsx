56be23c7b699e1c2618abb2b4d9e130ea6626fb8
Jay-bhrama_the_creator <jayeshupma6437@gmail.com>
2026-09-10 23:58:14 +0530

    Implement button click to open Google Docs link
    Added onClick handler to open a Google Docs link in a new tab.

--- src/components/AnimatedBorderButton.jsx
+++ src/components/AnimatedBorderButton.jsx
@@ -3,6 +3,7 @@ import { Download } from "lucide-react";
 export const AnimatedBorderButton = ({ children }) => {
   return (
     <button
+      onClick={() => window.open("https://docs.google.com/document/d/12Pt4xJvKSTfU3isUsPc5yicNXtBdi2An/edit?usp=drive_link&ouid=113977213988572837946&rtpof=true&sd=true", "_blank")}
       className="relative bg-transparent border border-border 
         text-foreground hover:border-primary/50 transition-all 
         duration-1000 focus:outline-none focus-visible:ring-2 
